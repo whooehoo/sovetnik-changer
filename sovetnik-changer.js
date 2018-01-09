71c065d11bb67a557acd4af03be8e22cb8a88b5a
whooehoo <whooehoo@yandex.ru>
2018-01-09 19:21:47 +0300

    :art: Export function

--- sovetnik-changer.js
+++ sovetnik-changer.js
@@ -1,20 +1,20 @@
-function sovetnikChanger(backgroundColor, backgroundImage) {
-  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+export default function sovetnikChanger(backgroundColor, backgroundImage) {
+  const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
   if (!MutationObserver) return;
 
-  var observer = new MutationObserver(function (mutations) {
+  const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (!mutation.addedNodes.length) return;
-      var addedNode = mutation.addedNodes[0];
+      const addedNode = mutation.addedNodes[0];
 
       if (addedNode && (addedNode.nodeType === 1) && (addedNode.tagName === 'DIV') && addedNode.id && addedNode.className && (addedNode.id === addedNode.className)) {
-        var firstNodeChild = addedNode.firstChild;
+        const firstNodeChild = addedNode.firstChild;
         if (firstNodeChild) {
           if (!backgroundColor) backgroundColor = '#fff';
-          var css = '.sovetnik-changer:after{content:""!important;position:absolute!important;top:0!important;right:0!important;left:0!important;bottom:-1px!important;z-index:2147483647!important;';
-          if (backgroundImage && (typeof backgroundImage === 'string')) css += 'background:'+ backgroundColor+' url("'+ backgroundImage + '") center no-repeat/contain!important;cursor:pointer!important;}';
-          else css += 'background-color:'+backgroundColor+'!important;cursor:default!important;}';
-          var style = document.createElement('style');
+          let css = '.sovetnik-changer:after{content:""!important;position:absolute!important;top:0!important;right:0!important;left:0!important;bottom:-1px!important;z-index:2147483647!important;';
+          if (backgroundImage && (typeof backgroundImage === 'string')) css += 'background:' + backgroundColor + ' url("' + backgroundImage + '") center no-repeat/contain!important;cursor:pointer!important;}';
+          else css += 'background-color:' + backgroundColor + '!important;cursor:default!important;}';
+          const style = document.createElement('style');
           style.type = 'text/css';
           document.head.appendChild(style);
           style.appendChild(document.createTextNode(css));
@@ -28,5 +28,3 @@ function sovetnikChanger(backgroundColor, backgroundImage) {
 
   observer.observe(document.body, {childList: true});
 }
-
-sovetnikChanger();
